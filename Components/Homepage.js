a49fc16d8419f279b4d29ecf53a5fd5cceab4be6
sikanderc <sikanderchowhan@gmail.com>
2018-04-05 18:01:16 -0400

    made the login screen

--- Components/Homepage.js
+++ Components/Homepage.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
-import DHClogo from '../Images/DHC-logo-white.png'
+import { View, Button, Alert, Text, Image } from 'react-native';
+import DHClogo from '../Images/DHC-wordmark-white.png'
 
 export default class Homepage extends Component {
   render() {
 
     return (
-      <Image source={DHClogo} style={{width: 205, height: 240}}/>
+      <View style={{flex: 1, alignItems:"center"}}>
+        <Image style={{padding: 30, flex: 3}} source={DHClogo} aspectRatio=".9" resizeMode="contain" />
+        <Text style={{flex: 1, padding: 10, fontSize: 25, textAlign: "center"}}>
+          Doctors for Healthy Communities
+        </Text>
+        <Text style={{flex: 1, fontSize: 20, textAlign: "center"}}>
+          Volunteer Log
+        </Text>
+        <Button style={{flex: 2, padding: 10}} color="#e86386" title="Login/Sign up"
+          onPress={() => {
+            Alert.alert('You tapped the button!');
+          }}
+        />
+        <Text style={{textAlign: "center", padding: 20}}>
+          © Doctors for Healthy Communities 2018
+        </Text>
+      </View>
     );
   }
 }
